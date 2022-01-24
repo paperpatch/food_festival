7fac3cf4a52327b4ea984fa47b2c078f554ad0ae
paperpatch <patchen21@gmail.com>
2022-01-24 14:52:45 -0500

    added image optimization loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,9 @@ module.exports = {
                 return url.replace("../", "/assets/")
               }
             }
+          },
+          {
+            loader: 'image-webpack-loader'
           }
         ]
       }
